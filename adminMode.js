fea6beea8c6eed8e89b3084d7f0bffb8d8158e81
rare1k <hi@rare1k.dev>
2024-03-02 10:39:56 -0500

    Create adminMode.js

---
+++ adminMode.js
@@ -0,0 +1,2 @@
+// Enables some developer things, and allows you to preview modals. (Simulating lessons does not work)
+Object.values(document.getElementById("StudentDashboard-g38"))[1].children[0]._owner.stateNode.props.dispatchToggleCheatButtonsAction();
